f47b1f39127cdafc782b41071c6dbc63b5dd879b
Oleg Tarasenko <oltarasenko@gmail.com>
2019-05-14 00:23:15 +0300

    Extend Crawly.Request with documentation

--- lib/crawly/request.ex
+++ lib/crawly/request.ex
@@ -1,3 +1,21 @@
 defmodule Crawly.Request do
+  @moduledoc """
+  Request wrapper
+
+  Defines Crawly request structure.
+  """
   defstruct url: nil, headers: [], prev_response: nil, options: []
+
+  @type header() :: {key(), value()}
+  @typep key :: binary()
+  @typep value :: binary()
+
+  @type option :: {atom(), binary()}
+
+  @type t :: %__MODULE__{
+    url: binary(),
+    headers: [header()],
+    prev_response: %{},
+    options: [option()]
+  }
 end
