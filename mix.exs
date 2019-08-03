be21033f867d9bb0d25d62c6c542e38738ac6dab
Oleg Tarasenko <oltarasenko@gmail.com>
2019-08-03 21:38:57 +0200

    Update application version to 0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Crawly.Mixfile do
   def project do
     [
       app: :crawly,
-      version: "0.1.0",
+      version: "0.1.1",
       name: "Crawly",
       source_url: "https://github.com/oltarasenko/crawly",
       elixir: "~> 1.7",
