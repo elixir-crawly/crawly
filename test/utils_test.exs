0c78d1356335fe34f6c62a39724b4fc1285dda45
Oleg Tarasenko <oltarasenko@gmail.com>
2019-08-10 17:16:58 +0200

    Add tests for build absolute urls utils

--- test/utils_test.exs
+++ test/utils_test.exs
@@ -18,4 +18,16 @@ defmodule UtilsTest do
              %Crawly.Request{url: "https://example.com", headers: []}
            ]
   end
+
+  test "Build absolute url test" do
+    url = Crawly.Utils.build_absolute_url("/url1", "http://example.com")
+    assert url == "http://example.com/url1"
+  end
+
+  test "Build absolute urls test" do
+    paths = ["/path1", "/path2"]
+    result = Crawly.Utils.build_absolute_urls(paths, "http://example.com")
+
+    assert result == ["http://example.com/path1", "http://example.com/path2"]
+  end
 end
