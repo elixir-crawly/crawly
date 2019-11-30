e6c6ebeab0427c6989d45e3cba79c01d370de09b
ziinc <tzeyiing@gmail.com>
2019-11-30 11:09:03 +0800

    added failing test for nested value behaviour

--- test/pipelines/csv_encoder_test.exs
+++ test/pipelines/csv_encoder_test.exs
@@ -2,6 +2,7 @@ defmodule Pipelines.CSVEncoderTest do
   use ExUnit.Case, async: false
 
   @valid %{first: "some", second: "data"}
+  @nested_valid %{first: "some", second: [%{some: "data1", other: "data2"}]}
 
   test "CSVEncoder converts a single-level map to a csv string" do
     Application.put_env(:crawly, :item, [:first, :second])
@@ -16,4 +17,16 @@ defmodule Pipelines.CSVEncoderTest do
     assert item == ~S("some","data")
   end
 
+  test "CSVEncoder converts a nested map to a csv string and flattens anything beyond the first level to json" do
+    Application.put_env(:crawly, :item, [:first, :second])
+
+    pipelines = [Crawly.Pipelines.CSVEncoder]
+    item = @valid
+    state = %{}
+
+    {item, _state} = Crawly.Utils.pipe(pipelines, item, state)
+
+    assert is_binary(item)
+    assert item == ~S("some","[{\"some\":\"data1\"},{\"other\":\"data2\"}]")
+  end
 end
