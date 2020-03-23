80ff93ce59ec9ef5a5c1178f3af6a77b54623b03
Ziinc <Ziinc@users.noreply.github.com>
2020-03-23 22:38:49 +0800

    added proxy examples, link to httpoison docs

--- lib/crawly/middlewares/request_options.ex
+++ lib/crawly/middlewares/request_options.ex
@@ -4,13 +4,23 @@ defmodule Crawly.Middlewares.RequestOptions do
 
   Allows to specify HTTP request settings like follow_redirect, or request
   timeout.
-
+  
+  If using `HTTPoisonFetcher` (the default), please refer to the [HTTPoison Request documentation](https://hexdocs.pm/httpoison/HTTPoison.Request.html#content) for full list of `:options`. 
+  
+  ## Example Usage
   ### Example Declaration
   ```
   middlewares: [
     {Crawly.Middlewares.RequestOptions, [timeout: 30_000, recv_timeout: 15000]}
   ]
   ```
+  ### Declaring proxy settings
+  ```
+  middlewares: [
+   {Crawly.Middlewares.RequestOptions, [proxy: {"https://my_host.com", 3000}, proxy_auth: {"my_user", "my_password}]}
+  ]
+  ```
+  
   """
   @behaviour Crawly.Pipeline
 
