f63f80aaef8cb7c0d50cc9b30e5f0cfeea20ecf5
Michal Trzcinka <michal@trzcinka.pl>
2020-04-21 15:52:15 +0200

    Handle empty fields config in CSVEncoder

--- lib/crawly/pipelines/csv_encoder.ex
+++ lib/crawly/pipelines/csv_encoder.ex
@@ -17,13 +17,8 @@ defmodule Crawly.Pipelines.CSVEncoder do
   @spec run(map, map, fields: list(atom)) ::
           {false, state :: map} | {csv_line :: String.t(), state :: map}
   def run(item, state, opts \\ []) do
-    opts = Enum.into(opts, %{fields: nil})
-    fields = Map.get(opts, :fields)
-
-    case fields do
-      nil ->
-        # only for when both tuple and global config is not provided
-
+    case opts[:fields] do
+      fields when fields in [nil, []] ->
         Logger.error(
           "Dropping item: #{inspect(item)}. Reason: No fields declared for CSVEncoder"
         )
--- test/pipelines/csv_encoder_test.exs
+++ test/pipelines/csv_encoder_test.exs
@@ -1,24 +1,30 @@
 defmodule Pipelines.CSVEncoderTest do
   use ExUnit.Case, async: false
 
-  @valid %{first: "some", second: "data"}
+  @item %{first: "some", second: "data"}
+  @state %{}
+
   test "Converts a single-level map to a csv string with fields config" do
     pipelines = [{Crawly.Pipelines.CSVEncoder, fields: [:first, :second]}]
-    item = @valid
-    state = %{}
 
-    {item, _state} = Crawly.Utils.pipe(pipelines, item, state)
+    {item, _state} = Crawly.Utils.pipe(pipelines, @item, @state)
 
     assert is_binary(item)
     assert item == ~S("some","data")
   end
 
+  test "Drops an item if fields are empty" do
+    pipelines = [{Crawly.Pipelines.CSVEncoder, fields: []}]
+
+    {item, _state} = Crawly.Utils.pipe(pipelines, @item, @state)
+
+    assert item == false
+  end
+
   test "Drops an item if fields are not declared" do
     pipelines = [{Crawly.Pipelines.CSVEncoder}]
-    item = @valid
-    state = %{}
 
-    {item, _state} = Crawly.Utils.pipe(pipelines, item, state)
+    {item, _state} = Crawly.Utils.pipe(pipelines, @item, @state)
 
     assert item == false
   end
