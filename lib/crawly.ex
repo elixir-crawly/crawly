aeaeef74cbddfd5f3a7b4fa1a25a2de645c956ce
ziinc <tzeyiing@gmail.com>
2020-05-18 17:49:05 +0800

    Added with option, moved all optional parameters to list opt

--- lib/crawly.ex
+++ lib/crawly.ex
@@ -9,32 +9,74 @@ defmodule Crawly do
 
   The fetched URL is being converted to a request, and the request is piped
   through the middlewares specidied in a config (with the exception of
-  `Crawly.Middlewares.DomainFilter`, `Crawly.Middlewares.RobotsTxt` these 2 are
-  ignored)
+  `Crawly.Middlewares.DomainFilter`, `Crawly.Middlewares.RobotsTxt`)
+
+  Provide a spider with the `:with` option to fetch a given webpage using that spider.
+
 
   """
-  @spec fetch(url, headers, options) :: HTTPoison.Response.t()
+
+  @spec fetch(url, opts) :: HTTPoison.Response.t()
         when url: binary(),
-             headers: [],
-             options: []
-  def fetch(url, headers \\ [], options \\ []) do
-    request0 = Crawly.Request.new(url, headers, options)
+             opts: list()
+  def fetch(url, opts \\ []) do
+    opts = Enum.into(opts, %{with: nil, request_options: [], headers: []})
+
+    request0 =
+      Crawly.Request.new(url, opts[:headers], opts[:request_options])
+      |> Map.put(
+        :middlewares,
+        Crawly.Utils.get_settings(:middlewares, opts[:with], [])
+      )
+
     ignored_middlewares = [
       Crawly.Middlewares.DomainFilter,
       Crawly.Middlewares.RobotsTxt
     ]
-    middlewares = request0.middlewares -- ignored_middlewares
 
-    {request, _} = Crawly.Utils.pipe(middlewares, request0, %{})
+    new_middlewares = request0.middlewares -- ignored_middlewares
 
-    {fetcher, client_options} = Application.get_env(
-      :crawly,
-      :fetcher,
-      {Crawly.Fetchers.HTTPoisonFetcher, []}
-    )
+    request0 =
+      Map.put(
+        request0,
+        :middlewares,
+        new_middlewares
+      )
+
+    {%{} = request, _} = Crawly.Utils.pipe(request0.middlewares, request0, %{})
+
+    {fetcher, client_options} =
+      Crawly.Utils.get_settings(
+        :fetcher,
+        opts[:with],
+        {Crawly.Fetchers.HTTPoisonFetcher, []}
+      )
 
     {:ok, response} = fetcher.fetch(request, client_options)
-    response
+
+    case opts[:with] do
+      nil ->
+        # no spider provided, return response as is
+        response
+
+      _ ->
+        # spider provided, send response through  parse_item callback, pipe through the pipelines
+        with parsed_result <- parse(response, opts[:with]),
+             pipelines <-
+               Crawly.Utils.get_settings(
+                 :pipelines,
+                 opts[:with]
+               ),
+             items <- Map.get(parsed_result, :items, []),
+             pipeline_result <-
+               Enum.reduce(items, [], fn item, acc ->
+                 {piped, _state} = Crawly.Utils.pipe(pipelines, item, %{})
+
+                 [acc | piped]
+               end) do
+          {response, parsed_result, pipeline_result}
+        end
+    end
   end
 
   @doc """
@@ -49,13 +91,16 @@ defmodule Crawly do
     case Kernel.function_exported?(spider, :parse_item, 1) do
       false ->
         {:error, :spider_not_found}
+
       true ->
         spider.parse_item(response)
     end
   end
 
   @doc """
-  Returns a list of known modules which implements Crawly.Spider behaviour
+  Returns a list of known modules which implements Crawly.Spider behaviour.
+
+  Should not be used for spider management. Use functions defined in `Crawly.Engine` for that.
   """
   @spec list_spiders() :: [module()]
   def list_spiders(), do: Crawly.Utils.list_spiders()
--- test/crawly_test.exs
+++ test/crawly_test.exs
@@ -2,7 +2,38 @@ defmodule CrawlyTest do
   use ExUnit.Case
   doctest Crawly
 
-  test "greets the world" do
-    assert :test == :test
+  setup do
+    :meck.new(CrawlyTestSpider)
+
+    :meck.expect(CrawlyTestSpider, :parse_items, fn resp ->
+      %{
+        items: ["hello"],
+        requests: [
+          Crawly.Utils.request_from_url("https://www.example.com/test")
+        ]
+      }
+    end)
+
+    on_exit(fn ->
+      :meck.unload(CrawlyTestSpider)
+    end)
+  end
+
+  test "fetch/1 is able to fetch a given url using global config, returns a response" do
+    assert %HTTPoison.Response{} = Crawly.fetch("https://example.com")
+  end
+
+  test "fetch/2 with :with option provided returns the response, parsed_item result, and processed ParsedItems" do
+    assert {%HTTPoison.Response{}, parsed_items_res, parsed_items} =
+             Crawly.fetch("http://example.com", with: CrawlyTestSpider)
+
+    assert %{
+             items: items,
+             requests: requests
+           } = parsed_items_res
+
+    assert is_list(parsed_items)
+    assert length(parsed_items) == 1
+    assert ["hello"] = parsed_items
   end
 end
