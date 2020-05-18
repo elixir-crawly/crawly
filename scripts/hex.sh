671798a8db3cc04258cff045518577a718cc52d5
Oleg Tarasenko <oltarasenko@gmail.com>
2020-05-18 10:48:39 +0200

    Update version to 0.10.0

--- scripts/hex.sh
+++ scripts/hex.sh
@@ -7,6 +7,7 @@ echo '{api_key, <<"'${API_KEY}'">>}.' > ~/.hex/hex.config
 
 MIX_ENV=dev mix deps.get
 
+MIX_ENV=dev mix generate_documentation
 MIX_ENV=dev mix hex.publish package --yes
 MIX_ENV=dev mix hex.publish docs --yes
 MIX_ENV=dev mix clean
