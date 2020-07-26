fde83b31b72630ff3532134f9f7cc565065d7272
Sreenadh TC <kesav.tc8@gmail.com>
2020-07-26 22:01:25 +0530

    Add fake user agents when no other ua is configured

--- lib/crawly/middlewares/user_agent.ex
+++ lib/crawly/middlewares/user_agent.ex
@@ -1,4 +1,6 @@
 defmodule Crawly.Middlewares.UserAgent do
+  alias Faker.Internet.UserAgent, as: FakeUserAgent
+
   @moduledoc """
   Set/Rotate user agents for crawling. The user agents are read from
   :crawly, :user_agents sessions.
@@ -21,16 +23,40 @@ defmodule Crawly.Middlewares.UserAgent do
   def run(request, state, opts \\ []) do
     opts = Enum.into(opts, %{user_agents: nil})
 
+    device_type = Map.get(request, :device_type, :desktop)
+
     new_headers = List.keydelete(request.headers, "User-Agent", 0)
 
-    user_agents =
-      Map.get(opts, :user_agents, ["Crawly Bot 1.0"])
-
-    useragent = Enum.random(user_agents)
+    useragent =
+      case Map.get(opts, :user_agents) do
+        nil -> choose_fake_useragent(device_type)
+        [] -> choose_fake_useragent(device_type)
+        useragents -> Enum.random(useragents)
+      end
 
     new_request =
       Map.put(request, :headers, [{"User-Agent", useragent} | new_headers])
 
     {new_request, state}
+  end
+
+  defp choose_fake_useragent(:desktop) do
+    FakeUserAgent.desktop_user_agent()
+  end
+
+  defp choose_fake_useragent(:mobile) do
+    FakeUserAgent.mobile_user_agent()
+  end
+
+  defp choose_fake_useragent(:game_console) do
+    FakeUserAgent.game_console_user_agent()
+  end
+
+  defp choose_fake_useragent(:tablet) do
+    FakeUserAgent.tablet_user_agent()
+  end
+
+  defp choose_fake_useragent(:ereader) do
+    FakeUserAgent.ereader_user_agent()
   end
 end
