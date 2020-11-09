b5ccb62695e63160e10a38c6507d079f88f6d391
Oleg Tarasenko <oltarasenko@gmail.com>
2020-11-09 20:49:14 +0100

    Extend domain filter with "share link" urls
    Looks like urls like https://twitter.com?link=our_link was not
    filtered out before. I have added filter for this and tests

--- lib/crawly/middlewares/domain_filter.ex
+++ lib/crawly/middlewares/domain_filter.ex
@@ -19,8 +19,9 @@ defmodule Crawly.Middlewares.DomainFilter do
 
   def run(request, state, _opts \\ []) do
     base_url = state.spider_name.base_url()
+    parsed_url = URI.parse(request.url)
 
-    case String.contains?(request.url, base_url) do
+    case String.contains?(base_url, parsed_url.host) do
       false ->
         Logger.debug(
           "Dropping request: #{inspect(request.url)} (domain filter)"
