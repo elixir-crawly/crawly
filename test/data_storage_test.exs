f4e2271c5364fee93054850cfc032afe8ebb7ba8
Ziinc <ty@tzeyiing.com>
2021-01-28 21:52:26 +0800

    consolidated data storage tets

--- test/data_storage_test.exs
+++ test/data_storage_test.exs
@@ -1,75 +1,68 @@
 defmodule DataStorageTest do
   use ExUnit.Case, async: false
-
+  @name "my_data_storage_test_spider"
+  @other_name "diff_spider"
+  @item %{
+    title: "test title",
+    author: "me",
+    time: "Now",
+    url: "http://example.com"
+  }
+  @crawl_id "my_crawl_id"
   setup do
-    name = :test_crawler
-    {:ok, pid} = Crawly.DataStorage.start_worker(name, "id")
+    # create a mock pipeline
+    :meck.new(PipelineMock, [:non_strict])
+
+    :meck.expect(Crawly.Utils, :get_settings, fn :pipelines, _spider_name, _ ->
+      [PipelineMock]
+    end)
+
+    {:ok, pid} = Crawly.DataStorage.start_worker(@name, @crawl_id)
 
     on_exit(fn ->
+      :meck.unload()
+
       :ok =
         DynamicSupervisor.terminate_child(Crawly.DataStorage.WorkersSup, pid)
     end)
 
-    {:ok, %{crawler: name}}
+    :ok
   end
 
-  test "Can store data item", context do
-    Crawly.DataStorage.store(context.crawler, %{
-      title: "test title",
-      author: "me",
-      time: "Now",
-      url: "http://example.com"
-    })
+  describe "pipeline works" do
+    setup do
+      :meck.expect(PipelineMock, :run, fn item, state ->
+        {item, state}
+      end)
+    end
 
-    {:stored_items, 1} = Crawly.DataStorage.stats(context.crawler)
+    test "Can store data item" do
+      assert :ok = Crawly.DataStorage.store(@name, @item)
+      assert {:stored_items, 1} = Crawly.DataStorage.stats(@name)
+      assert :meck.history(PipelineMock) |> length() == 1
+    end
   end
 
-  test "Duplicates are not stored", context do
-    Crawly.DataStorage.store(context.crawler, %{
-      title: "test title",
-      author: "me",
-      time: "Now",
-      url: "http://example.com"
-    })
-
-    Crawly.DataStorage.store(context.crawler, %{
-      title: "test title",
-      author: "me",
-      time: "Now",
-      url: "http://example.com"
-    })
-
-    {:stored_items, 1} = Crawly.DataStorage.stats(context.crawler)
-  end
-
-  test "Items without all required fields are dropped", context do
-    Crawly.DataStorage.store(context.crawler, %{
-      author: "me",
-      time: "Now",
-      url: "http://example.com"
-    })
-
-    {:stored_items, 0} = Crawly.DataStorage.stats(context.crawler)
-  end
-
-  test "Items without all required fields are dropped nils", context do
-    Crawly.DataStorage.store(context.crawler, %{
-      title: "title",
-      author: nil,
-      time: "Now",
-      url: "http://example.com"
-    })
-
-    {:stored_items, 0} = Crawly.DataStorage.stats(context.crawler)
-  end
-
-  test "Starting child worker twice", context do
-    result = Crawly.DataStorage.start_worker(context.crawler, "id")
+  test "Starting child worker twice returns error tuple" do
+    result = Crawly.DataStorage.start_worker(@name, @crawl_id)
     assert result == {:error, :already_started}
   end
 
-  test "Stats for not running spiders" do
-    result = Crawly.DataStorage.stats(:unkown)
-    assert result == {:error, :data_storage_worker_not_running}
+  test "Stats for not running spiders returns error tuple" do
+    assert {:error, :data_storage_worker_not_running} =
+             Crawly.DataStorage.stats(@other_name)
+  end
+
+  describe "pipeline drops" do
+    setup do
+      :meck.expect(PipelineMock, :run, fn _item, state ->
+        {false, state}
+      end)
+    end
+
+    test "Dropped item are not stored" do
+      Crawly.DataStorage.store(@name, @item)
+      {:stored_items, 0} = Crawly.DataStorage.stats(@name)
+    end
   end
 end
