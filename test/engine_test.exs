33766680566f1f8271ffb10c6485b1560d1f749c
Ziinc <ty@tzeyiing.com>
2021-01-28 17:57:52 +0800

    updated engine tests to reflect async operations

--- test/engine_test.exs
+++ test/engine_test.exs
@@ -4,30 +4,43 @@ defmodule EngineTest do
 
   setup do
     on_exit(fn ->
-      Engine.stop_spider(TestSpider)
+      Engine.list_started_spiders()
+      |> Enum.map(fn info ->
+        Engine.stop_spider(info.name)
+      end)
     end)
   end
+
+  @name Atom.to_string(TestSpider)
 
   test "list_started_spiders/0 lists all spiders" do
     assert [] = Engine.list_started_spiders()
     Engine.start_spider(TestSpider)
     assert [started] = Engine.list_started_spiders()
-    assert is_pid(started.pid)
-    Engine.stop_spider(TestSpider)
-    assert [] = Engine.list_known_spiders()
+    assert started.status == :initializing
+    assert started.pid == nil
+    assert :ok == Engine.stop_spider(TestSpider)
+    :timer.sleep(50)
+    assert [] = Engine.list_started_spiders()
   end
 
   test "list_spider_templates/0 lists all spider template modules" do
-    assert [] = Engine.list_spider_templates()
     Engine.refresh_spider_list()
     assert [TestSpider] = Engine.list_spider_templates()
+  end
+
+  test "get_spider_info/1 returns spider info if started" do
+    assert nil == Engine.get_spider_info(TestSpider)
+    Engine.start_spider(TestSpider)
+    assert %{name: @name} = Engine.get_spider_info(@name)
+    assert %{name: @name} = Engine.get_spider_info(TestSpider)
   end
 
   test "start_spider/2 with :name option creates a runtime spider using a template module" do
     assert :ok = Engine.start_spider(TestSpider, name: "Test Spider")
 
-    assert [%{name: "Test Spider", status: :started, template: template}] =
-             Engine.list_known_spiders()
+    assert [%{name: "Test Spider", template: template}] =
+             Engine.list_started_spiders()
 
     assert template == TestSpider
   end
@@ -36,38 +49,40 @@ defmodule EngineTest do
     assert :ok = Engine.start_spider(TestSpider)
 
     # stringify the module name
-    assert [%{name: "TestSpider", template: template}] =
-             Engine.list_known_spiders()
-
+    assert [%{name: name, template: template}] = Engine.list_started_spiders()
+    assert Atom.to_string(template) == name
     assert TestSpider == template
   end
 
   test "start_spider/2 without :name option returns error if existing name is taken" do
     assert :ok = Engine.start_spider(TestSpider)
-    assert {:error, :already_started} = Engine.start_spider(TestSpider)
+    assert {:error, :spider_already_started} = Engine.start_spider(TestSpider)
   end
 
   test "start_spider/2 with clashing :name option returns error" do
     assert :ok = Engine.start_spider(TestSpider, name: "SomeSpider")
 
-    assert {:error, :already_started} =
+    assert {:error, :spider_already_started} =
              Engine.start_spider(TestSpider, name: "SomeSpider")
   end
 
   test "stop_spider/1 with string name stops spider with matching name" do
     assert :ok = Engine.start_spider(TestSpider, name: "Test Spider")
-
+    :timer.sleep(100)
     assert :ok = Engine.stop_spider("Test Spider")
-    assert [] = Engine.list_known_spiders()
+    :timer.sleep(100)
+    assert [] = Engine.list_started_spiders()
   end
 
   test "stop_spider/1 with template name stops all spiders using that template" do
     assert :ok = Engine.start_spider(TestSpider, name: "Test Spider")
     assert :ok = Engine.start_spider(TestSpider, name: "Test Spider 2")
-    assert Engine.list_known_spiders() |> length() == 2
+    assert Engine.list_started_spiders() |> length() == 2
+    :timer.sleep(100)
 
     assert :ok = Engine.stop_spider(TestSpider)
-    assert [] = Engine.list_known_spiders()
+    :timer.sleep(100)
+    assert [] = Engine.list_started_spiders()
   end
 
   describe "crawl_id tagging" do
@@ -82,9 +97,15 @@ defmodule EngineTest do
       assert Engine.get_crawl_id("myspider")
     end
 
-    test "Engine will use a tag from external system if set" do
+    test "can manually set crawl_id with other options" do
       tag = "custom_crawl_tag"
-      :ok = Engine.start_spider(TestSpider, tag)
+      :ok = Engine.start_spider(TestSpider, tag, name: "my_other_spider")
+      assert {:ok, tag} == Engine.get_crawl_id("my_other_spider")
+    end
+
+    test "can manually set crawl_id option" do
+      tag = "custom_crawl_tag"
+      :ok = Engine.start_spider(TestSpider, crawl_id: tag)
       assert {:ok, tag} == Engine.get_crawl_id(TestSpider)
     end
   end
