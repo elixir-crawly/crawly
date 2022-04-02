37e0eb2933b7bb0a9ba696a6ec4f39fd51c2b701
Matteo Redaelli <matteo.redaelli@gmail.com>
2022-04-02 15:45:30 +0200

    Adding a test about removing fragment from url

--- test/middlewares/unique_request_test.exs
+++ test/middlewares/unique_request_test.exs
@@ -3,6 +3,8 @@ defmodule Middlewares.UniqueRequestTest do
 
   @valid %Crawly.Request{url: "https://www.some_url.com"}
 
+  @valid_with_fragment %Crawly.Request{url: "https://www.some_url.com#frag1"}
+
   test "Filters out requests non-unique urls" do
     middlewares = [Crawly.Middlewares.UniqueRequest]
     req = @valid
@@ -11,5 +13,10 @@ defmodule Middlewares.UniqueRequestTest do
     assert {_req, state} = Crawly.Utils.pipe(middlewares, req, state)
     # run again, should drop the request
     assert {false, _state} = Crawly.Utils.pipe(middlewares, req, state)
+
+    req_with_fragment = @valid_with_fragment
+
+    # should drop the request
+    assert {false, _state} = Crawly.Utils.pipe(middlewares, req_with_fragment, state)
   end
 end
