b1c08e3558c4ac91391f3f825b4150f84f7f9092
TzeYiing <ty@tzeyiing.com>
2022-05-28 01:10:47 +0100

    chore: remove failing basic test

--- apps/crawldis_web/test/crawldis_web/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule CrawldisWeb.PageControllerTest do
-  use CrawldisWeb.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
-  end
-end
