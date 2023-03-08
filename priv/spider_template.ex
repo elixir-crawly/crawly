5e6ec5846942f6a2de7d2dfbf187dacdf77369ae
oltarasenko <oltarasenko@gmail.com>
2023-03-08 10:02:28 +0100

    Spider and Configuration generators (#240)
    The current commit introduces two mix commands which are supposed to speed
    up spider/configuration creation for Crawly projects.
    
    This is one of the features used in Scrapy, and it allows you to make spiders
    almost instantly withjust one command. It simplifies my work, and hopefully, it will be helpful for others as well!
    
    Mix files are not included into coverage reports

---
+++ priv/spider_template.ex
@@ -0,0 +1,35 @@
+defmodule SpiderTemplate do
+  use Crawly.Spider
+
+  @impl Crawly.Spider
+  def base_url(), do: "https://books.toscrape.com/"
+
+  @impl Crawly.Spider
+  def init() do
+    [start_urls: ["https://books.toscrape.com/index.html"]]
+  end
+
+  @impl Crawly.Spider
+  @doc """
+     Extract items and requests to follow from the given response
+  """
+  def parse_item(response) do
+    # Extract item field from the response here. Usually it's done this way:
+    # {:ok, document} = Floki.parse_document(response.body)
+    # item = %{
+    #   title: document |> Floki.find("title") |> Floki.text()
+    #   url: response.request_url
+    # }
+    extracted_items = []
+
+    # Extract requests to follow from the response. Don't forget that you should
+    # supply request objects here. Usually it's done via
+    #
+    # urls = document |> Floki.find(".pagination a") |> Floki.attribute("href")
+    # Don't forget that you need absolute urls
+    # requests = Crawly.Utils.requests_from_urls(urls)
+
+    next_requests = []
+    %Crawly.ParsedItem{items: extracted_items, requests: next_requests}
+  end
+end
