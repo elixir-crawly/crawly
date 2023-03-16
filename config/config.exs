245d54a1b6353c327c4c49ad5a9bc65f8847eead
oltarasenko <oltarasenko@gmail.com>
2023-03-16 10:26:14 +0100

    Cleanup unused options from config samples (#242)

--- config/config.exs
+++ config/config.exs
@@ -2,33 +2,6 @@
 # and its dependencies with the aid of the Mix.Config module.
 import Config
 
-# This configuration is loaded before any dependency and is restricted
-# to this project. If another project depends on this project, this
-# file won't be loaded nor affect the parent project. For this reason,
-# if you want to provide default values for your application for
-# 3rd-party users, it should be done in your "mix.exs" file.
-
-# You can configure your application as:
-#
-#     config :crawly, key: :value
-#
-# and access this configuration in your application as:
-#
-#     Application.get_env(:crawly, :key)
-#
-# You can also configure a 3rd-party app:
-#
-#     config :logger, level: :info
-#
-
-# It is also possible to import configuration files, relative to this
-# directory. For example, you can emulate configuration per environment
-# by uncommenting the line below and defining dev.exs, test.exs and such.
-# Configuration from the imported file will override the ones defined
-# here (which is why it is important to import them last).
-#
-#     import_config "#{Mix.env}.exs"
-
 config :crawly,
   fetcher: {Crawly.Fetchers.HTTPoisonFetcher, []},
   retry: [
@@ -42,10 +15,6 @@ config :crawly,
   # Stop spider if it does crawl fast enough
   closespider_timeout: 20,
   concurrent_requests_per_domain: 5,
-
-  # TODO: this looks outdated
-  follow_redirect: true,
-  log_to_file: false,
 
   # Request middlewares
   middlewares: [
